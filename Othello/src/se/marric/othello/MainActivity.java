3d23ff97be23214116e7466c7c5b213f56b7135d
Marcus <mac.jonsson@gmail.com>
2013-04-05 21:23:16 +0200

    added start activity

--- Othello/src/se/marric/othello/MainActivity.java
+++ Othello/src/se/marric/othello/MainActivity.java
@@ -1,7 +1,8 @@
 package se.marric.othello;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -9,7 +10,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        startGame();
     }
 
 
@@ -18,6 +19,11 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+    
+    private void startGame() {
+        Intent i = new Intent(this, StartActivity.class);
+        startActivity(i);
     }
     
 }
