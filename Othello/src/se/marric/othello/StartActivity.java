852fe2da9b120fdc0f93056c828c4296d27255d0
Marcus <mac.jonsson@gmail.com>
2013-04-05 22:50:37 +0200

    added listener to start game button

--- Othello/src/se/marric/othello/StartActivity.java
+++ Othello/src/se/marric/othello/StartActivity.java
@@ -1,19 +1,29 @@
 package se.marric.othello;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class StartActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		Intent i = new Intent(this, GameActivity.class);
-        startActivity(i);
-	}
+		setContentView(R.layout.activity_start);
+		
+		
+		final Button button = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	startGame();
+                // Perform action on click
+            }
+        });
 
+        }
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -21,4 +31,8 @@ public class StartActivity extends Activity {
 		return true;
 	}
 
+	public void startGame() {
+		 Intent i = new Intent(this, GameActivity.class);
+	        startActivity(i);
+	}
 }
